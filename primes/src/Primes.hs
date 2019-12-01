3cb9f9b4bc9f6ea7da96f88c9a078b781a4a9b2a
Ilya Igonkin <osdev0@gmail.com>
2019-12-01 17:01:46 +0400

    Add (37) Building a prime-number library

---
+++ primes/src/Primes.hs
@@ -0,0 +1,28 @@
+module Primes where
+
+primes :: [Int]
+primes = sieve [2 .. 10000]
+-- primes = sieve [2 .. ]
+
+isPrime :: Int -> Maybe Bool
+isPrime n | n < 2 = Nothing
+          | n >= length primes = Nothing
+          | otherwise = Just (n `elem` primes)
+
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+    where noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:primes) = if n `mod` next == 0
+                                     then next : unsafePrimeFactors (n `div` next) (next:primes)
+                                     else unsafePrimeFactors n primes
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors n | n < 2 = Nothing
+               | n >= length primes = Nothing
+               | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+    where primesLessThanN = filter (<= n) primes
